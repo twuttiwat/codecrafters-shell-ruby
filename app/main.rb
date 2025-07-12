becb804eb70e35e2f7e85c0496cddb01b76e4741
Teerawat Wuttiwat <781147+twuttiwat@users.noreply.github.com>
2025-07-12 20:09:05 +0700

    codecrafters submit [skip ci]

--- app/main.rb
+++ app/main.rb
@@ -1,14 +1,33 @@
 EXIT_CMD = "exit"
+ECHO_CMD = "echo"
 
-while true do
-  # Get input command
+def write_prompt
   $stdout.write("$ ")
-  command, *args = gets.chomp.split(" ")
+end
 
-  if command == EXIT_CMD
-    break
+def get_input
+  gets.chomp.split(" ")
+end
+
+def handle_command(command, *args)
+  case command
+  when EXIT_CMD
+    return true
+  when ECHO_CMD
+    puts args.join(" ")
+  else
+    puts "#{command}: command not found"
   end
+end
 
-  # Evaluate command
-  $stdout.write("#{command}: command not found\n")
+def do_exit(args)
+  exit_val = args.first.to_i || 0
+  exit exit_val
+end
+
+while true
+  write_prompt
+  command, *args = get_input
+  is_exit = handle_command(command, args)
+  do_exit(args) if is_exit
 end
