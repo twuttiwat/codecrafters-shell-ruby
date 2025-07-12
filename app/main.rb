d3916986818895f6bf4db62760a3caad60436687
Teerawat Wuttiwat <781147+twuttiwat@users.noreply.github.com>
2025-07-12 19:39:20 +0700

    codecrafters submit [skip ci]

--- app/main.rb
+++ app/main.rb
@@ -1,7 +1,13 @@
+EXIT_CMD = "exit"
+
 while true do
   # Get input command
   $stdout.write("$ ")
   command, *args = gets.chomp.split(" ")
+
+  if command == EXIT_CMD
+    break
+  end
 
   # Evaluate command
   $stdout.write("#{command}: command not found\n")
