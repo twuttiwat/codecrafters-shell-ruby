9d113a8793f1c5eea2c2d6f0a9da6b6dd58e50eb
Teerawat Wuttiwat <781147+twuttiwat@users.noreply.github.com>
2025-07-14 21:30:41 +0700

    codecrafters submit [skip ci]

--- app/main.rb
+++ app/main.rb
@@ -1,5 +1,6 @@
 EXIT_CMD = "exit"
 ECHO_CMD = "echo"
+TYPE_CMD = "type"
 
 def write_prompt
   $stdout.write("$ ")
@@ -9,12 +10,24 @@ def get_input
   gets.chomp.split(" ")
 end
 
-def handle_command(command, *args)
+def is_builtin(command)
+  builtin_cmds = [EXIT_CMD, ECHO_CMD, TYPE_CMD]
+  builtin_cmds.include? command
+end
+
+def handle_command(command, args)
   case command
   when EXIT_CMD
     return true
   when ECHO_CMD
     puts args.join(" ")
+  when TYPE_CMD
+    cmd_to_check = args.first
+    if is_builtin(cmd_to_check)
+      puts "#{cmd_to_check} is a shell builtin" if is_builtin(cmd_to_check)
+    else 
+      puts "#{cmd_to_check}: not found"
+    end
   else
     puts "#{command}: command not found"
   end
